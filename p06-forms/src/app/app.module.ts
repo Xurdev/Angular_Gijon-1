40301926dcdacfd6a1682582899f24af986f6162
alce65 <alce65@hotmail.es>
2017-11-15 17:12:31 +0100

    Formularios reactivos

--- p06-forms/src/app/app.module.ts
+++ p06-forms/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { MainModule } from './main/main.module';
@@ -12,6 +13,7 @@ import { MainModule } from './main/main.module';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     MainModule,
     SharedModule
   ],
